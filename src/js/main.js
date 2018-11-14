77a8cde703b18d946b474be5d87d0a9aafefc11d
dovganych <misha.dovganych@gmail.com>
2018-11-14 17:12:34 +0200

    fix service worker path

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,6 @@ quiz.processQuiz();
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('../sw_cached_pages.js');
+      .register('sw_cached_pages.js');
   });
 }
